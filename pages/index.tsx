fbcd2badf2aeacfebee0b7531f19efa7485e180a
Ben Schmidt <ben@schmidtfam.us>
2022-10-28 17:33:44 -0600

    Updated desc and projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,9 @@ export default function Home() {
 
 				<p className={styles.description}>
 					Hi! I&apos;m Ben, a 3rd-year Computer Science student at the
-					University of Calgary.
+					University of Calgary with a concentration in Networks and Distributed Computing.<br/>
+					I&apos;m interested in web development and computer networks, and am currently managing a team of 7 developers as part of a software club.
+					<br/>Seeking co-op/internship positions for 2023.
 				</p>
 
 				<h1 className={styles.title}>Projects</h1>
@@ -36,23 +38,34 @@ export default function Home() {
 					<Card
 						title="Portfolio"
 						link="https://github.com/anidion/portfolio"
-						description="The page you're on!"
-						flipdesc="Flipside"
+						description="The website you're on!"
+						flipdesc="These cards can flip for more information! Technical Details: Built with React and TypeScript, hosted on Vercel."
+						isDark={theme.palette.mode == "dark" ? true : false}
+					/>
+
+					<Card
+						title="CyberHire"
+						link="https://github.com/techstartucalgary/cyberhire"
+						description="A platform for employers and applicants to connect based on skills required by a job."
+						flipdesc="I am the project manager for CyberHire, and I'm managing a team of 7. Technical Details: Built with React, TypeScript, and FlaskAPI, hosted on GitHub Pages and Amazon RDS."
 						isDark={theme.palette.mode == "dark" ? true : false}
 					/>
 
 					<Card
 						title="Aquavolution"
-						link="https://github.com/tech-start-ucalgary/aquavolution"
+						link="https://github.com/techstartucalgary/aquavolution"
 						description="A browser-based 2D game where you play as a fish who must navigate
 						the ocean, avoiding danger and evolving as you eat more and more."
+						flipdesc="I was a developer on the Aquavolution team, and I worked primarily on the movement and procedural generation systems.
+						Technical Details: Built with Unity and C#."
 						isDark={theme.palette.mode == "dark" ? true : false}
 					/>
 
 					<Card
 						title="Raytracer"
 						link="https://github.com/anidion/raytracer"
-						description="A C++ raytracer. Built thanks to Peter Shirley's Ray Tracing in One Weekend."
+						description="A C++ raytracer."
+						flipdesc="Built thanks to Peter Shirley's Ray Tracing in One Weekend. I haven't got very far, but I'd like to make more progress once I find the time."
 						isDark={theme.palette.mode == "dark" ? true : false}
 					/>
 
@@ -61,6 +74,7 @@ export default function Home() {
 						link="https://github.com/anidion/password-manager"
 						description="A functional browser-based password manager built for a final
 						project in a security and privacy class."
+						flipdesc="I built this project with a team of 4, and I worked primarily on the front-end. No promises that it's super secure, so please don't store your actual passwords! Technical Details: Built with React, JavaScript, and MongoDB, hosted on Vercel and Heroku."
 						isDark={theme.palette.mode == "dark" ? true : false}
 					/>
 				</div>
@@ -122,7 +136,10 @@ export default function Home() {
 							href="https://drive.google.com/file/d/1d0FfUxD_vgxqGDBiI2zrGJsgVOL74tPy/view?usp=sharing"
 							className={styles.logo}
 						>
-							<ArticleRoundedIcon fontSize="large" color="success" />
+							<ArticleRoundedIcon
+								fontSize="large"
+								color="success"
+							/>
 						</IconButton>
 					</Tooltip>
 
